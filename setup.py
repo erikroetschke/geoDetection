d21bd758a2ca5851ba6752f8b0897172f22a735e
erikroetschke <66082999+erikroetschke@users.noreply.github.com>
2023-07-14 11:25:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="geoDetection",
+      version="1.0.0",
+      description="implements geo data structures and algorithms for analysing privacy in geo data",
+      author="Erik Rötschkel",
+      authoer_email="info@erik-roetschke.de",
+      url='https://github.com/erikroetschke/geoDetection',
+      packages=['geoDetection']
+)
